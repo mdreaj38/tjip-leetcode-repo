dd80b34066ca07a5f92ea429a0ca7dec3337c058
mdreaj38 <30681563+mdreaj38@users.noreply.github.com>
2022-04-09 01:54:57 +0600

    Update threeSum.cpp

--- week01/threeSum.cpp
+++ week01/threeSum.cpp
@@ -5,12 +5,11 @@ vector<vector<int>> threeSum(vector<int>& nums) {
         for(int i=0;i<nums.size();++i){
             if(i and nums[i]==nums[i-1]) continue;
             
-            int l = i+1, r= (int)nums.size()-1;
+            int l = i+1, r = (int)nums.size()-1;
             while(l<r){
                 int sum = nums[l]+nums[r]+nums[i];
                 if(sum == 0){
-                     
-                    ans.push_back({nums[l],nums[r],nums[i]});
+                    ans.push_back({nums[l], nums[r],nums[i]});
                     
                     while(l<r and nums[l]==nums[l+1]) l++;
                     l++;
