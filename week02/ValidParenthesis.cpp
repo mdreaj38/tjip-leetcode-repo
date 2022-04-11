e726a9d5fe9c58a75298a3d57530726c4b013730
mdreaj38 <30681563+mdreaj38@users.noreply.github.com>
2022-04-11 12:23:38 +0600

    Update ValidParenthesis.cpp

--- week02/ValidParenthesis.cpp
+++ week02/ValidParenthesis.cpp
@@ -1,27 +1,28 @@
-class Solution {
-public:
-    bool isValid(string s) {
-        stack<char>openPar;
+bool isValid(string s)
+{
+    stack<char> openPar;
 
-        for(char chr:s){
-    	    if(openPar.empty() or chr=='(' or chr=='{' or chr=='[')
-    	    	openPar.push(chr);
-    	    else{
-    	    	char temp = openPar.top();
-		if(chr!=getCloseBracket(temp)) 
-			return false;
-			
-		openPar.pop();
-            }
+    for (char chr : s)
+    {
+        if (openPar.empty() or chr == '(' or chr == '{' or chr == '[')
+            openPar.push(chr);
+        else
+        {
+            char temp = openPar.top();
+            if (chr != getCloseBracket(temp))
+                return false;
+
+            openPar.pop();
         }
-
-        return openPar.empty();        
     }
 
-    char getCloseBracket(char ch){
-    	if(ch=='(') return ')';
-    	if(ch=='{') return '}';
-	    
-        return ']';
-    }
-};
+    return openPar.empty();
+}
+
+char getCloseBracket(char ch)
+{
+    if (ch == '(') return ')';
+    if (ch == '{') return '}';
+
+    return ']';
+}
