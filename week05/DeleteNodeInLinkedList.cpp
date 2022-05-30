fb359b8d60ca9f23bd68890d4b1822c5d176434f
mdreaj38 <30681563+mdreaj38@users.noreply.github.com>
2022-05-30 17:13:03 +0600

    Added 6 solutions

---
+++ week05/DeleteNodeInLinkedList.cpp
@@ -0,0 +1,9 @@
+// TC: O(1)
+// MC: O(1)
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
---
+++ week05/copyListWithRandomPointer.cpp
@@ -0,0 +1,26 @@
+
+//TC: O(N)
+//MC: O(1)
+class Solution {
+public:	
+    Node* copyRandomList(Node* head) {
+		unordered_map<Node*, Node*> oldToClone;
+		Node* curPos = head;
+
+		while(curPos){
+			oldToClone[curPos] = new Node(curPos->val);
+			curPos = curPos->next;
+		}
+		
+		curPos = head;
+		while(curPos){
+			Node* cloneNode = oldToClone[curPos];
+			cloneNode->next = oldToClone[curPos->next];
+			cloneNode->random = oldToClone[curPos->random];
+            
+			curPos = curPos->next;
+		}
+
+		return oldToClone[head];
+	}
+};
